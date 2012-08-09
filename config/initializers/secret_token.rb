0f92f875a5363b8157103bcd8102f6e6c842fb17
Kori Roys <kori@koriroys.com>
2012-08-09 11:47:59 -0500

    barebones rails 3.2.7 project, no tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DelinquentCoder::Application.config.secret_token = '424ad74fd0221ba0e2f877b550c2bc9311d193da6c3a994d145d5bd8720b60375959b61d464bced50e0974ddfcd29b833fc23279d12b68a728607d5449308e42'
